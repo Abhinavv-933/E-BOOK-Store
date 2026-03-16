16bb7765182e5c9aff1e2426f38dd1f0b66df37c
Abhinav Dwivedi <143184201+Abhinavv-933@users.noreply.github.com>
2026-03-17 01:17:46 +0530

    feat: enhance AdminDashboard with book fetching, deletion, and improved loading state

--- frontend/src/pages/Admin/components/dashboard/adminDashboard.js
+++ frontend/src/pages/Admin/components/dashboard/adminDashboard.js
@@ -1,9 +1,173 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
+import { deleteBook, getBooks } from "../../service/admin";
+import { Grid, Typography,Box , Button, Paper, CircularProgress, Backdrop} from "@mui/material";
+import { Edit as  EditIcon, Delete as DeleteIcon} from '@mui/icons-material';
+import { styled } from "@mui/material/styles";
 
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    height: '250px',
+    objectFit: 'cover'
+});
+
+const Item =styled(Paper)(({ theme }) => ({
+    backgroundColor: '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    ...theme.applyStyles('dark', {
+       backgroundColor: '#1A2027' ,
+    }),
+}))
 
 export default function AdminDashboard() {
-    const [book,setBooks] = useState();
+    const [books,setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const {enqueueSnackbar } = useSnackbar();
+
+    const fetchBooks = async () => {
+        setLoading(true);
+        try {
+          const response  = await getBooks();
+          if(response.status === 200){
+            setBooks(response.data);
+          }
+        } catch (error) {
+          console.error(error.message);
+        } finally{
+          setLoading(false);
+        }
+      };
+
+      useEffect(() => {
+        fetchBooks();
+      },[]);
+
+
+      const handleDeleteBook = async (id) => {
+        setLoading(true);
+    
+        try {
+          const response  = await deleteBook(id);
+          if(response.status === 200){
+            navigate(`/admin/dashboard`)
+            enqueueSnackbar('Book deleted successfully', {variant: 'success', autoHideDuration:5000});
+          }
+        } catch (error) {
+          enqueueSnackbar('Getting error while deleting book', {variant: 'error', autoHideDuration:5000});
+        } finally{
+          setLoading(false);
+        }
+      };
+
+    const handleUpdateBook = async (id) => {
+       navigate(`/admin/book/${id}/edit`);
+    };
+
+
     return (
-       <h1> Admin Dashboard</h1>
+      <>
+        <Box sx={{ flexGrow:1, p:5}}>
+          <Grid container spacing= {2}>
+             {books.map((book) => (
+                 <Grid size={{ xs: 12, md: 6 }} key={book._id}>
+                   <Item>
+                      <Box sx={{ dispay: 'flex',p:3 , alignItems: 'center'}}>
+                        <Box sx={{ width: '40%', display: 'flex', justifyContent: 'center',p:2}}>
+                                <Img alt="complex" src={book.imageUrl} sx={{ width:'100%' , height: 'auto', maxWidth: '150px'}} />
+                          </Box>
+                          <Box sx={{ width: '60%',pl:3}}>
+                          <Typography varient="h6" component="div">
+                              <strong>{book.title}</strong>
+                          </Typography>
+                          <Box sx={{ display: 'grid', gridTemplateColumns: '100px 1fr' , gap:1, mt:2}}>
+                              <Typography varient="body2" color="text.secondary">
+                                 Author
+                              </Typography>
+                              <Typography varient="body2" color="text.secondary">
+                                  <strong>{book.author}</strong>
+                              </Typography>
+
+                              <Typography varient="body2" color="text.secondary">
+                                  Description:
+                              </Typography>
+                              <Typography varient="body2" color="text.secondary">
+                                  <strong>{book.description}</strong>
+                              </Typography>
+
+                              <Typography varient="body2" color="text.secondary">
+                                  Price:
+                              </Typography>
+                              <Typography varient="body2" color="text.secondary">
+                                  <strong>{book.price}</strong>
+                              </Typography>
+
+                              <Typography varient="body2" color="text.secondary">
+                                  Genre:
+                              </Typography>
+                              <Typography varient="body2" color="text.secondary">
+                                  <strong>{book.genre}</strong>
+                              </Typography>
+
+                              <Typography varient="body2" color="text.secondary">
+                                  Condition:
+                              </Typography>
+                              <Typography varient="body2" color="text.secondary">
+                                  <strong>{book.condition}</strong>
+                              </Typography>
+
+                              <Typography varient="body2" color="text.secondary">
+                                  Edition:
+                              </Typography>
+                              <Typography varient="body2" color="text.secondary">
+                                  <strong>{book.edition}</strong>
+                              </Typography>
+
+                              <Typography varient="body2" color="text.secondary">
+                                  Status:
+                              </Typography>
+                              <Typography varient="body2" color="text.secondary">
+                                  <strong>{book.status}</strong>
+                              </Typography>
+                          </Box>
+                          <Box sx={{disply: 'flex',gap:2 , mt:2}}>
+                               <Button
+                                   varient="outlined"
+                                   color="primary"
+                                   endIcon={<EditIcon />}
+                                   onClick={() => handleUpdateBook(book._id)}
+                                >
+                                  Update
+                                </Button>
+                                <Button
+                                   varient="outlined"
+                                   color="primary"
+                                   endIcon={<DeleteIcon />}
+                                   onClick={() => handleDeleteBook(book._id)}
+                                >
+                                  Delete
+                                </Button>
+                          </Box>
+                        </Box>
+                      </Box>
+                   </Item>
+                 </Grid>
+             ))}
+          </Grid>
+        </Box>
+
+        <Backdrop 
+         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+         open={loading}
+         >
+         <CircularProgress color="success" />
+        </Backdrop>
+      </>
     )
 }
--- frontend/src/pages/Admin/service/admin.js
+++ frontend/src/pages/Admin/service/admin.js
@@ -2,7 +2,26 @@ import instance from "../../../environment/axiosInstance";
 
 export const postBook = async(bookDto) => {
   try {
-    const response = instance.post('/api/admin/book',bookDto);
+    const response = await instance.post('/api/admin/book',bookDto);
+    return response;
+  } catch (error) {
+      throw error;
+  }
+}
+
+export const deleteBook = async(id) => {
+  try {
+    const response = await instance.delete(`/api/admin/book/${id}`);
+    return response;
+  } catch (error) {
+      throw error;
+  }
+}
+
+
+export const getBooks = async() => {
+  try {
+    const response = await instance.get('/api/admin/book');
     return response;
   } catch (error) {
       throw error;
