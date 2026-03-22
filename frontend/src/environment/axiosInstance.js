c83e199cdc8ff915e2aacd7000511dffc2857724
Abhinav Dwivedi <143184201+Abhinavv-933@users.noreply.github.com>
2026-03-22 20:53:30 +0530

    fixed baseURL , CORS Issue

--- frontend/src/environment/axiosInstance.js
+++ frontend/src/environment/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {getToken} from "../utils/common";
 
 const instance = axios.create({
-   baseURL: 'http://localhost:5000', // Change this to your backend port
+   baseURL: process.env.VITE_API_URL || 'http://localhost:5000', // Change this to your backend port
    headers: {
      'Content-Type': 'application/json',
    },
